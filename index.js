82634ce0107d63d1cb1abbe954ed6d7bd834ac92
Melvin Finn <melvinrf@Outlook.com>
2021-05-17 11:46:01 -0500

    added the correct flow of the prompt

--- index.js
+++ index.js
@@ -4,40 +4,31 @@ inquirer.registerPrompt('recursive', require('inquirer-recursive'));
 inquirer
   .prompt([
     {
-      type: 'recursive',
-      message: 'Add a new user?',
-      name: 'users',
-      prompts: [
-        {
-          type: 'input',
-          message: "'What is the employee's name?",
-          name: 'name', 
-        },
-        {
-          type: 'list',
-          message: "'What is the employee's position?'",
-          name: 'position',
-          choices: ['Manager', 'Engineer', 'Intern',]
-        },
-        {
-          type: 'input',
-          message: "'What is the employee's id'",
-          name: 'id', 
-        },
-        {
-          type: 'input',
-          message: "'What is the employee's email?'",
-          name: 'email',
-        },
-        {
-          type: 'input',
-          message: "'What is the employee's Github username?'",
-          name: 'github',
-        }]
+      type: 'input',
+      message: "'What is the manager's name?'",
+      name: 'mName',
+    },
+    {
+      type: 'input',
+      message: "'What is the manager's employee ID?'",
+      name: 'mID',
+    },
+    {
+      type: 'input',
+      message: "'What is the manager's email address?'",
+      name: 'mEmail',
+    },
+    {
+      type: 'input',
+      message: "'What is the manager's office number?'",
+      name: 'mOn',
+    },
+    {
+      type: 'list',
+      name: 'list',
+      message: "WHich type of team member would you like to add?",
+      choices: ['Engineer', 'Intern', "I'm done making my team!"],
     },
   ])
-  .then((response) =>
-    response.confirm === response.password
-      ? console.log('Success!')
-      : console.log('You forgot your password already?!')
-  );
+
+
