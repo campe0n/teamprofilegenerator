ab04461720823bf98ad3abbb13835cd2a0f8301b
Melvin Finn <melvinrf@Outlook.com>
2021-05-21 22:39:32 -0500

    created handlr

--- index.js
+++ index.js
@@ -1,34 +1,59 @@
+const Employee = require('./lib/employee')
+const Engineer = require('./lib/engineer')
+const Intern = require('./lib/intern')
+const Manager = require('./lib/manager');
 const inquirer = require('inquirer');
 
-inquirer.registerPrompt('recursive', require('inquirer-recursive'));
-inquirer
-  .prompt([
-    {
-      type: 'input',
-      message: "'What is the manager's name?'",
-      name: 'mName',
-    },
-    {
-      type: 'input',
-      message: "'What is the manager's employee ID?'",
-      name: 'mID',
-    },
-    {
-      type: 'input',
-      message: "'What is the manager's email address?'",
-      name: 'mEmail',
-    },
-    {
-      type: 'input',
-      message: "'What is the manager's office number?'",
-      name: 'mOn',
-    },
-    {
-      type: 'list',
-      name: 'list',
-      message: "WHich type of team member would you like to add?",
-      choices: ['Engineer', 'Intern', "I'm done making my team!"],
-    },
-  ])
 
+var position = "manager's"
 
+const getInfo = () => {
+    return inquirer.prompt([
+      {
+        type: 'input',
+        message: `What is the ${position} name?`,
+        name: 'name',
+      },
+      {
+        type: 'input',
+        message: `What is the ${position} ID?`,
+        name: 'id',
+      },
+      {
+        type: 'input',
+        message: `What is the ${position} email?`,
+        name: 'email',
+      },
+      {
+        type: 'input',
+        message: `What is the ${position} officeNumber?`,
+        name: 'officeNumber',
+      },
+      {
+        type: 'list',
+        message: 'Which role would you like to add',
+        name: 'list',
+        choices: ['Engineer', 'Intern', "I'm done making my team!"]
+      }])
+      .then((answers) => handler(answers))
+  }
+
+const handler = (answers) => {
+    if (answers.list === 'Engineer') {
+      position = "Engineer's"
+      const engineer = new Engineer()
+      getInfo();
+      console.log(engineer)
+    } else if (answers.list === 'Intern') {
+      position = "Intern's"
+      const intern = new Intern()
+      getInfo();
+      console.log(intern)
+    } else (console.log('done with team'))
+}
+
+const init = () => {
+    getInfo()
+}
+
+init()
