5d0ccbe5f1ba1f36797df0d254c643d03b1f1fde
Melvin Finn <melvinrf@Outlook.com>
2021-05-22 21:29:59 -0500

    finished getting info now working on html

--- index.js
+++ index.js
@@ -4,66 +4,147 @@ const Intern = require('./lib/intern')
 const Manager = require('./lib/manager');
 const inquirer = require('inquirer');
 
+var employees = [];
 
-var position = "manager's"
-
-const getInfo = () => {
-    return inquirer.prompt([
+function getManager() {
+    const managerq = [
       {
         type: 'input',
-        message: `What is the ${position} name?`,
+        message: `What is the manager's name?`,
         name: 'name',
       },
       {
         type: 'input',
-        message: `What is the ${position} ID?`,
+        message: `What is the manager's ID?`,
         name: 'id',
       },
       {
         type: 'input',
-        message: `What is the ${position} email?`,
+        message: `What is the manager's email?`,
         name: 'email',
       },
       {
         type: 'input',
-        message: `What is the ${position} officeNumber?`,
-        name: 'officeNumber',
-      },
-      {
-        type: 'list',
-        message: 'Which role would you like to add',
-        name: 'list',
-        choices: ['Engineer', 'Intern', "I'm done making my team!"]
-      }])
-      .then((answers) => handler(answers))
+        message: `What is the manager's office number?`,
+        name: 'officeNo',
+      }]
+      inquirer.prompt(managerq).then((data) => {
+        var manager = new Manager(data.name, data.id, data.email, data.officeNo)
+        employees.push(manager)
+        console.log(employees)
+        handler();
+      })
   }
 
-const handler = (answers) => {
-    if (answers.list === 'Engineer') {
-      position = "Engineer's"
-      var engineer = new Engineer(answers.name, answers.id, answers.email);
-      getInfo();
-      const promise = new Promise((resolve, reject) => {
-        setTimeout(() => {
-          resolve('foo');
-        }, 300);
-      });
-      promise
-      .then(getInfo())
-      engineer.getGithub();
-    } else if (answers.list === 'Intern') {
-      position = "Intern's"
-      var intern = new Intern()
-      getInfo();
+function getEngineer() {
+    const engineerq = [
+      {
+        type: 'input',
+        message: `What is the engineer's name?`,
+        name: 'name',
+      },
+      {
+        type: 'input',
+        message: `What is the engineer's ID?`,
+        name: 'id',
+      },
+      {
+        type: 'input',
+        message: `What is the engineer's email?`,
+        name: 'email',
+      },
+      {
+        type: 'input',
+        message: `What is the engineer's github?`,
+        name: 'github',
+      }]
+      inquirer.prompt(engineerq).then((data) => {
+        var engineer = new Engineer(data.name, data.id, data.email, data.github)
+        employees.push(engineer)
+        handler();
+      })
+  }
+
+  function getIntern() {
+    const internq = [
+      {
+        type: 'input',
+        message: `What is the intern's name?`,
+        name: 'name',
+      },
+      {
+        type: 'input',
+        message: `What is the intern's ID?`,
+        name: 'id',
+      },
+      {
+        type: 'input',
+        message: `What is the intern's email?`,
+        name: 'email',
+      },
+      {
+        type: 'input',
+        message: `What is the intern's school?`,
+        name: 'school',
+      }]
+      inquirer.prompt(internq).then((data) => {
+        var intern = new Intern(data.name, data.id, data.email, data.school)
+        employees.push(intern)
+        handler();
+      })
+  }
+
+function handler() {
+  inquirer.prompt([
+    {
+      type: 'list',
+      message: 'What position would you like to add?',
+      choices: ['Intern','Engineer',"I'm done making my team!"],
+      name: 'list',
+    },
+    ])
+    .then((data) => {
+      if (data.list === 'Engineer') {
+      getEngineer();
+    } else if (data.list === 'Intern') {
+      getIntern();
     } else {
-      console.log('done with team')
-      console.log(engineer)
-      console.log(intern)
-    }
+      generateCards();
+    } 
+  })
 }
 
+function generateCards() {
+  writeToFile("./dist/index.html", generateHTML(employees));
+}
+
+function writeToFile(filename, data) {
+  false.writeFile(filename, data, (err) =>
+  err ? console.log(err) : console.log("commit logged"))
+  console.log(employees)
+}
+
+function generateHTML(employees) {
+  `<!DOCTYPE html>
+  <html lang="en">
+  <head>
+      <meta charset="UTF-8">
+      <meta http-equiv="X-UA-Compatible" content="IE=edge">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <title>Document</title>
+  </head>
+  <body>
+      
+  </body>
+  </html>`
+}
+
+
+
+
+
 const init = () => {
-  getInfo()
+  getManager()
 }
 
 init()
